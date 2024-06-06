f1a47aafc17b8a57ff02ffcb7de6ab1a3f4432f8
Prakhar Bhawsar <bhawsarprakhar2003@gmail.com>
2024-06-07 04:45:16 +0530

    phone number auth added

--- app/log-in/page.js
+++ app/log-in/page.js
@@ -1,17 +1,23 @@
 'use client'
 import { useState, useEffect } from 'react';
-import {signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from 'firebase/auth';
+import {signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult, RecaptchaVerifier, signInWithPhoneNumber   } from 'firebase/auth';
 import {auth} from '@/firebase.js'
 import { useRouter } from 'next/navigation';
 import toast from "react-hot-toast";
 import Cookies from "js-cookie";
 import Link from 'next/link';
+import PhoneInput from "react-phone-input-2";
+import 'react-phone-input-2/lib/style.css'
 
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter()
+
+  const [phone, setPhone] = useState("")
+  const [user, setUser] = useState(null)
+  const [otp, setOtp] = useState("")
 
   useEffect(() => {
     getUserData()
@@ -81,8 +87,40 @@ const SignUp = () => {
     }
   }
 
+  const sendOTP = async () => {
+    try {
+      const recaptcha = new RecaptchaVerifier(auth, "recaptchaID", {})
+      const confirmation = await signInWithPhoneNumber(auth, phone, recaptcha)
+      .then((confirmationResult) => {
+        // console.log(confirmationResult)
+        setUser(confirmationResult)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const verifyOTP = async () => {
+    try {
+      const data = await user.confirm(otp)
+      .then((result) => {
+        console.log(result.user.uid)
+        toast.success("Logged In With Phone Number Successfully")
+        Cookies.set("loggedInCookie", true)
+        router.push('/')
+      })
+      .catch((error) => {
+        console.log(error)
+      })      
+    } catch (error) {
+      console.error(err)
+    }
+  }
   return (
-    <div className="flex items-center justify-center bg-gray-900 my-10">
+    <div className="flex items-center justify-center bg-gray-900 my-5">
       <div className="bg-gray-800 p-5 sm:p-10 rounded-lg shadow-lg w-11/12 sm:w-1/3 border-2 shadow-blue-400 flex flex-col items-center gap-2">
         <h1 className="text-white text-2xl mb-5 font-bold">Log In</h1>
         <input 
@@ -90,14 +128,14 @@ const SignUp = () => {
           placeholder="Email" 
           value={email} 
           onChange={(e) => setEmail(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+          className="w-full p-3 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <input 
           type="password" 
           placeholder="Password" 
           value={password} 
           onChange={(e) => setPassword(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+          className="w-full p-3 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <button 
           onClick={handleSignUp}
@@ -110,7 +148,7 @@ const SignUp = () => {
 
 
         {/* LOG IN WITH POP UP */}
-        <button onClick= {googleSignIn} className='flex h-14  w-full border-2 rounded-lg items-center pr-1 hover:bg-white mb-2'>
+        <button onClick= {googleSignIn} className='flex h-14  w-full border-2 rounded-lg items-center pr-1 hover:bg-white'>
           <img src="/google-icon.png" alt="G" className='h-full p-1 rounded-lg'/>
           <div className="w-full p-4 h-12 flex items-center justify-center bg-indigo-600 rounded text-white ">
             Log In with Google Pop Up
@@ -125,7 +163,25 @@ const SignUp = () => {
           </div>
         </button>
 
-        <p className='text-white'>or</p>
+        {/* Phone LOG IN */}
+        <div className='w-full'>
+          
+          <PhoneInput country={"in"} value={phone} onChange= {(phone) => setPhone("+" + phone)} className='mb-1 pl-4 sm:pl-16' />
+          <div id="recaptchaID" className='pl-4 sm:pl-16'></div>
+          <div className='flex h-10 gap-1'>
+            <button onClick={sendOTP} className="p-3 bg-indigo-600 rounded-lg w-1/4 flex items-center text-sm sm:text-base">Send OTP</button>
+            <input 
+            type="number" 
+            placeholder="Enter OTP" 
+            value={otp} 
+            onChange={(e) => setOtp(e.target.value)} 
+            className="p-3 bg-gray-700 rounded-lg outline-none text-white placeholder-gray-500 w-2/4"
+            />
+            <button onClick={verifyOTP} className="p-3 bg-indigo-600 rounded-lg w-1/4 flex items-center text-sm sm:text-base">Verify OTP</button>
+          </div>
+        </div>
+
+        {/* <p className='text-white'>or</p> */}
 
         <div className='flex justify-center gap-2 text-white'>
           Don't have an account? <Link href={'/sign-up'} className='text-blue-500'>Sign Up</Link>
--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -1,11 +1,13 @@
 'use client'
 import { useState, useEffect } from 'react';
-import {createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult  } from 'firebase/auth';
+import {createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult, RecaptchaVerifier, signInWithPhoneNumber  } from 'firebase/auth';
 import {auth} from '@/firebase.js'
 import { useRouter } from 'next/navigation';
 import toast from "react-hot-toast";
 import Cookies from "js-cookie";
 import Link from 'next/link';
+import PhoneInput from "react-phone-input-2";
+import 'react-phone-input-2/lib/style.css'
 
 
 const SignUp = () => {
@@ -13,6 +15,9 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   const router = useRouter()
   
+  const [phone, setPhone] = useState("")
+  const [user, setUser] = useState(null)
+  const [otp, setOtp] = useState("")
 
   useEffect(() => {
     getUserData()
@@ -50,7 +55,7 @@ const SignUp = () => {
             console.log(userData.user.uid + "POPUP")
             // setEmail('');
             // setPassword('');
-            toast.success("Success")
+            toast.success("Logged In With Pop UP Successfully")
             Cookies.set("loggedInCookie", true)
             router.push('/')
         })
@@ -78,32 +83,63 @@ const SignUp = () => {
     const response = await getRedirectResult(auth)
     if(response){
       console.log(response.user.uid + "REDIRECT")
-      toast.success("Success")
+      toast.success("Logged In With Redirect Successfully")
       Cookies.set("loggedInCookie", true)
       router.push('/')
     }
   }
 
+  const sendOTP = async () => {
+    try {
+      const recaptcha = new RecaptchaVerifier(auth, "recaptchaID", {})
+      const confirmation = await signInWithPhoneNumber(auth, phone, recaptcha)
+      .then((confirmationResult) => {
+        // console.log(confirmationResult)
+        setUser(confirmationResult)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
+  const verifyOTP = async () => {
+    try {
+      const data = await user.confirm(otp)
+      .then((result) => {
+        console.log(result.user.uid)
+        toast.success("Logged In With Phone Number Successfully")
+        Cookies.set("loggedInCookie", true)
+        router.push('/')
+      })
+      .catch((error) => {
+        console.log(error)
+      })      
+    } catch (error) {
+      console.error(err)
+    }
+  }
 
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-900 my-10">
+    <div className="flex flex-col items-center justify-center bg-gray-900 my-5">
       {/* SIGN UP WITH EMAIL PASS */}
-      <div className="bg-gray-800 p-5 sm:p-10 rounded-lg shadow-lg w-11/12 sm:w-1/3 border-2 shadow-blue-400  flex flex-col items-center gap-2">
+      <div className="bg-gray-800 p-3 sm:p-10 rounded-lg shadow-lg w-11/12 sm:w-1/3 border-2 shadow-blue-400  flex flex-col items-center gap-2">
         <h1 className="text-white text-2xl mb-5 font-bold">Sign Up</h1>
         <input 
           type="email" 
           placeholder="Email" 
           value={email} 
           onChange={(e) => setEmail(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+          className="w-full p-3 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <input 
           type="password" 
           placeholder="Password" 
           value={password} 
           onChange={(e) => setPassword(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+          className="w-full p-3 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <button 
           onClick={handleSignIn}
@@ -116,7 +152,7 @@ const SignUp = () => {
 
 
         {/* SIGN UP WITH POP UP */}
-        <button onClick= {googleSignIn} className='flex h-14  w-full border-2 rounded-lg items-center pr-1 hover:bg-white mb-2'>
+        <button onClick= {googleSignIn} className='flex h-14  w-full border-2 rounded-lg items-center pr-1 hover:bg-white'>
           <img src="/google-icon.png" alt="G" className='h-full p-1 rounded-lg'/>
           <div className="w-full p-4 h-12 flex items-center justify-center bg-indigo-600 rounded text-white ">
             Sign Up with Google Pop Up
@@ -131,7 +167,27 @@ const SignUp = () => {
           </div>
         </button>
 
-        <p className='text-white'>or</p>
+
+        {/* Phone SIGN UP */}
+        <div className='w-full'>
+          
+          <PhoneInput country={"in"} value={phone} onChange= {(phone) => setPhone("+" + phone)} className='mb-1 pl-4 sm:pl-16' />
+          <div id="recaptchaID" className='pl-4 sm:pl-16'></div>
+          <div className='flex h-10 gap-1'>
+            <button onClick={sendOTP} className="p-3 bg-indigo-600 rounded-lg w-1/4 flex items-center text-sm sm:text-base">Send OTP</button>
+            <input 
+            type="number" 
+            placeholder="Enter OTP" 
+            value={otp} 
+            onChange={(e) => setOtp(e.target.value)} 
+            className="p-3 bg-gray-700 rounded-lg outline-none text-white placeholder-gray-500 w-2/4"
+            />
+            <button onClick={verifyOTP} className="p-3 bg-indigo-600 rounded-lg w-1/4 flex items-center text-sm sm:text-base">Verify OTP</button>
+          </div>
+        </div>
+
+
+        {/* <p className='text-white'>or</p> */}
 
         <div className='flex justify-center gap-2 text-white'>
           Already have an account? <Link href={'/log-in'} className='text-blue-500'>Log In</Link>
